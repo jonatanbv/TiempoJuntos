9bab004e9c42aa9207cf657ffddddfaaae85d73d
jonatanbv <40503765+jonatanbv@users.noreply.github.com>
2023-03-22 14:14:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let html=document.getElementById('tiempo');
+
+        setInterval(function(){
+            tiempo = new Date();
+
+            horas = tiempo.getHours();
+            minutos = tiempo.getMinutes();
+            segundos = tiempo.getSeconds();
+            dias = tiempo.getDate();
+            dias = dias-4;
+
+            if (horas<10)
+                horas = "0"+horas;
+            if(minutos<10)
+                minutos = "0"+minutos;
+            if(segundos<10)
+                segundos = "0"+segundos;
+ 
+
+            html.innerHTML = dias+" : "+ horas+" : "+minutos+" : "+segundos;
+        },1000)
