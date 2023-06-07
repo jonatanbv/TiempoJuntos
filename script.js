1c536e0a753f72c67321a34ae90157b9704591ca
jonatanbv <40503765+jonatanbv@users.noreply.github.com>
2023-06-07 07:30:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let html=document.getElementById('tiempo');
             minutos = tiempo.getMinutes();
             segundos = tiempo.getSeconds();
             dias = tiempo.getDate();
-            dias = dias+57;
+            dias = dias+88;
 
             if (horas<10)
                 horas = "0"+horas;
